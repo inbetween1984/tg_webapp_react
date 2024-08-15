397c101fa2f5f481cdc49c70b39447062bba057a
denis <karlsn44@gmail.com>
2024-08-15 14:39:00 +0300

    Add hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import './App.css';
 import {useEffect} from "react";
-
+import {useTelegram} from "../hooks/useTelegram";
 
 function App() {
 
-  useEffect(() => {
-    tg.ready();
-  }, [])
+    const {onToggleButton, tg} = useTelegram();
 
 
+    useEffect(() => {
+        tg.ready();
+    }, [])
 
-  return (
-      <div className="App">
-        work
 
-      </div>
-  );
+    return (
+        <div className="App">
+            <button onClick={onToggleButton}>Toggle Telegram</button>
+        </div>
+    );
 }
 
 export default App;
